005a05892ebc4df6ac1eef18907a65a68dd16dbb
retrastore2 <146480009+retrastore2@users.noreply.github.com>
2023-10-01 19:49:13 -0300

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -33,7 +33,7 @@ async def answer(bot, query):
     if AUTH_CHANNEL and not await is_subscribed(bot, query):
         await query.answer(results=[],
                            cache_time=0,
-                           switch_pm_text='You have to subscribe my channel to use the bot',
+                           switch_pm_text='Veuillez rejoindre mon canal pour utiliser ce bot!',
                            switch_pm_parameter="subscribe")
         return
 
@@ -103,7 +103,7 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query)
+            InlineKeyboardButton('Chercher à nouveau', switch_inline_query_current_chat=query)
         ]
         ]
     return InlineKeyboardMarkup(buttons)
