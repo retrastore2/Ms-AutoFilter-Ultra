e9709e3f0cd71f59349aa0fcb1064e8b7f41663c
retrastore2 <146480009+retrastore2@users.noreply.github.com>
2023-10-02 22:57:35 -0300

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -49,7 +49,7 @@ async def save_file(media):
             caption=media.caption.html if media.caption else None,
         )
     except ValidationError:
-        logger.exception('Error occurred while saving file in database')
+        logger.exception('Une erreur s'est produite lors de l'enregistrement du fichier dans la base de données')
         return False, 2
     else:
         try:
